9d68402a72d97bde30201fc5c2c6f3af4bafb410
Johan Gunnarsson <johan.gunnarsson@gmail.com>
2016-04-24 20:28:07 +0200

    First PoC of everything working

--- src/mask.c
+++ src/mask.c
@@ -1,4 +1,25 @@
+/*
+    Copyright 2016 Johan Gunnarsson <johan.gunnarsson@gmail.com>
+
+    This file is part of AutoCut.
+
+    AutoCut is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AutoCut is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AutoCut.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "mask.h"
+
+#include <glib.h>
 
 #include <gst/gst.h>
 #include <gst/app/gstappsink.h>
@@ -8,12 +29,27 @@
 	"decodebin ! " \
 	"videoconvert ! " \
 	"video/x-raw,format=RGB ! " \
+	"queue ! " \
 	"appsink name=sink sync=false max-buffers=10"
 
 typedef struct {
 	gint width;
 	gint height;
+	GMainLoop *loop;
 } Stream;
+
+typedef struct {
+	Stream *stream;
+	GstMapInfo map;
+	gdouble timestamp;
+} StreamBuffer;
+
+typedef struct {
+	GMappedFile *file;
+	gchar *name;
+} Mask;
+
+static GList *masks = NULL;
 
 static gdouble
 get_timestamp(GstBuffer *buffer)
@@ -29,9 +65,56 @@ get_timestamp(GstBuffer *buffer)
 }
 
 static void
+compare_frames(gpointer data, gpointer user_data)
+{
+	// The mask to compare against
+	Mask *mask = (Mask *) data;
+
+	StreamBuffer *buf = (StreamBuffer *) user_data;
+
+	gsize masksz = g_mapped_file_get_length(mask->file);
+	gsize framesz = buf->map.size;
+
+	// Ignore this combination if they don't have exact same size
+	if (masksz != framesz)
+		return;
+
+	guint8 *maskdat = (guint8 *) g_mapped_file_get_contents(mask->file);
+	guint8 *framedat = buf->map.data;
+
+	guint sad = 0;
+	guint pixels = 0;
+
+	gint i = 0;
+
+	for (i = 0; i < masksz; i += 3) {
+		// Skip black pixels in mask
+		if (!maskdat[i + 0] && !maskdat[i + 0] && !maskdat[i + 0])
+			continue;
+
+		pixels++;
+
+		gint dr = maskdat[i + 0] - framedat[i + 0];
+		gint dg = maskdat[i + 1] - framedat[i + 1];
+		gint db = maskdat[i + 2] - framedat[i + 2];
+
+		// Averige absolute difference for R, G, B
+		sad += (ABS(dr) + ABS(dg) + ABS(db)) / 3;
+	}
+
+	gdouble score = ((gdouble) sad) / pixels;
+
+	// Print line if the mask is close enough to the frame. 6 is just an
+	// arbitrary number that seems to work well.
+	if (score < 6)
+		g_print("%s\t%0.1f\t%0.3f\n", mask->name, score,
+			buf->timestamp);
+}
+
+static void
 end_of_stream(GstAppSink *appsink, gpointer user_data)
 {
-	g_print("%s\n", __func__);
+	g_main_loop_quit(((Stream *) user_data)->loop);
 }
 
 static GstFlowReturn
@@ -51,7 +134,7 @@ new_preroll(GstAppSink *sink, gpointer user_data)
 	if (!gst_structure_get_int(structure, "height", &stream->height))
 		goto out;
 
-	g_print("Got preroll: %s %dx%d\n",
+	g_printerr("Pre-rolled video (format %s, resolution %dx%d)\n",
 		gst_structure_get_name(structure),
 		stream->width,
 		stream->height);
@@ -75,20 +158,45 @@ new_sample(GstAppSink *sink, gpointer user_data)
 	if (!buffer)
 		goto out;
 
-	GstMapInfo mapinfo;
+	StreamBuffer buf;
 
-	if (!gst_buffer_map(buffer, &mapinfo, GST_MAP_READ))
+	if (!gst_buffer_map(buffer, &buf.map, GST_MAP_READ))
 		goto out;
 
-	g_print("%ld %p %.3f\n", mapinfo.size, mapinfo.data,
-		get_timestamp(buffer));
+	buf.stream = (Stream *) user_data;
+	buf.timestamp = get_timestamp(buffer);
 
-	gst_buffer_unmap(buffer, &mapinfo);
+	g_list_foreach(masks, compare_frames, &buf);
+
+	gst_buffer_unmap(buffer, &buf.map);
 
 out:
 	gst_sample_unref(sample);
 
 	return GST_FLOW_OK;
+}
+
+static gboolean
+load_rgb(gchar *path, GError *error)
+{
+	GMappedFile *file = g_mapped_file_new(path, FALSE, &error);
+
+	if (!file)
+		return FALSE;
+
+	Mask *mask = g_malloc0(sizeof (Mask));
+
+	mask->file = file;
+	mask->name = g_path_get_basename(path);
+
+	// Put this mask at the end of the list of masks
+	masks = g_list_append(masks, mask);
+
+	g_printerr("Loaded RGB mask (name %s, size %lu)\n",
+		path,
+		g_mapped_file_get_length(file));
+
+	return TRUE;
 }
 
 static GstAppSinkCallbacks callbacks = {
@@ -104,11 +212,29 @@ main(int argc, char **argv)
 
 	g_set_application_name("mask");
 
+	if (!load_rgb("2-player-luigi-loser-mario-winner.rgb", error)) {
+		return 1;
+	}
+
+	if (!load_rgb("2-player-luigi-winner-mario-loser.rgb", error)) {
+		return 1;
+	}
+
+	if (!load_rgb("start.rgb", error)) {
+		return 1;
+	}
+
 	gst_init(&argc, &argv);
 
-	GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+	Stream *stream = g_malloc0(sizeof (Stream));
+
+	stream->loop = g_main_loop_new(NULL, FALSE);
 
 	GstElement *pipeline = gst_parse_launch(PIPELINE, &error);
+
+	if (!pipeline) {
+		return 1;
+	}
 
 	GstElement *mysink = gst_bin_get_by_name(GST_BIN(pipeline), "sink");
 	GstElement *mysrc = gst_bin_get_by_name(GST_BIN(pipeline), "src");
@@ -116,7 +242,7 @@ main(int argc, char **argv)
 	gst_app_sink_set_callbacks(
 		GST_APP_SINK(mysink),
 		&callbacks,
-		g_malloc0(sizeof (Stream)),
+		stream,
 		g_free);
 
 	g_object_set(
@@ -130,8 +256,11 @@ main(int argc, char **argv)
 
 	gst_element_set_state(GST_ELEMENT(pipeline), GST_STATE_PLAYING);
 
-	g_main_loop_run(loop);
-	g_main_loop_unref(loop);
+	g_main_loop_run(stream->loop);
+
+	g_object_unref(pipeline);
+
+	g_main_loop_unref(stream->loop);
 
 	return 0;
 }
